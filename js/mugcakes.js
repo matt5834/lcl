2a1e3d4c05906fb1c679e824c89e11476ad40c95
Katherine McConachie <kam635@mail.harvard.edu>
2014-06-25 14:14:18 -0400

    init

---
+++ js/mugcakes.js
@@ -0,0 +1,26 @@
+/* global $,Miso,JST */
+$(function(){
+
+  var cakes = new Miso.Dataset({
+    importer : Miso.Dataset.Importers.GoogleSpreadsheet,
+    parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
+    key: "1HFXxHu1kvEkgk1Hl65Sy_3jutddXoawupruDVgwPYI4",
+    worksheet: "0"
+  });
+
+  var workingColumns = [
+    "Project",
+    "Profile Link",
+    "URL for Gallery"
+  ];
+
+  var $el = $("#cakes");
+  cakes.fetch().done(function() {
+    cakes.each(function(cake) {
+       $el.append(JST["templates/cake.html"]({
+         cake : cake,
+         controls : workingColumns
+       }));
+    });
+  });
+}());
