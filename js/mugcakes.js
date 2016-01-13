2806fbc00ba6a5536facceebf1feedb7bfc5462c
matt5834 <mmclean@lrei.org>
2016-01-12 22:51:48 -0500

    Update mugcakes.js

--- js/mugcakes.js
+++ js/mugcakes.js
@@ -4,7 +4,7 @@ $(function(){
   var cakes = new Miso.Dataset({
     importer : Miso.Dataset.Importers.GoogleSpreadsheet,
     parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
-    key: "1HFXxHu1kvEkgk1Hl65Sy_3jutddXoawupruDVgwPYI4",  
+    key: "12B4GzljD0Tp8gbSpDcmGum7BP4vQEvsLQxd4Vbmbsjs",  
     worksheet: "1"
   });
 
